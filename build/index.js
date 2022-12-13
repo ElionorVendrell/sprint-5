ba99608b635ed97753b88773079eae3420661773
ElionorVendrell <elivendrell@gmail.com>
2022-12-13 12:18:27 +0100

    fix node module

--- build/index.js
+++ build/index.js
@@ -8,28 +8,61 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+//---> Variables
 const apiUrl = "https://icanhazdadjoke.com";
 const header = {
-    method: "GET",
-    headers: {
-        Accept: "application/json",
-    },
+    headers: { Accept: "application/json" },
 };
-const show = () => __awaiter(void 0, void 0, void 0, function* () {
-    const response = yield fetch(apiUrl, header);
-    const data = yield response.json();
-    const joke = data.joke;
-    console.log(joke); // no funciona
-    //document.getElementById("acudit").innerHTML = joke;
-    return joke;
+const mostrarAcudit = document.getElementById("acudit");
+const mostrarWeather = document.getElementById("weather");
+const showIcon = document.getElementById("icon");
+const boton = document.querySelectorAll(".btn");
+let currentJoke = "";
+const reportAcudits = [];
+const d = new Date();
+let date = d.toISOString();
+//---> 1a API Jokes
+const showJokes = () => __awaiter(void 0, void 0, void 0, function* () {
+    boton.forEach((e) => (e.disabled = false));
+    const response = yield (yield fetch(apiUrl, header)).json();
+    currentJoke = response.joke;
+    mostrarAcudit.innerHTML = currentJoke;
+    console.log(`api1 ${response.joke}`);
 });
-/* reportAcudits{
-
-    joke: "...",
-  
-    score: 1,
-  
-    date: ...
-  
-  
-  } */
+//---> 2a API Jokes (Chuk Norris)
+const showChuckNorrisJokes = () => __awaiter(void 0, void 0, void 0, function* () {
+    boton.forEach((e) => (e.disabled = false));
+    const response = yield (yield fetch("https://api.chucknorris.io/jokes/random")).json();
+    currentJoke = response.value;
+    mostrarAcudit.innerHTML = currentJoke;
+    console.log(`api2 ${response.value}`);
+});
+//---> Mostrar acudit alternant API
+let bool = true;
+const alterna = () => {
+    bool ? showJokes() : showChuckNorrisJokes();
+    bool = !bool;
+};
+//---> Puntuar acudits i mostrar per consola (puntuar només una vegada l'acudit)
+function getScore(id) {
+    reportAcudits.push({
+        joke: currentJoke,
+        score: id,
+        date: date,
+    });
+    boton.forEach((e) => (e.disabled = true));
+    console.log(reportAcudits);
+}
+//---> Mostrar el temps
+const url = "https://ipapi.co/json/";
+(() => __awaiter(void 0, void 0, void 0, function* () {
+    const { latitude, longitude } = yield (yield fetch(url)).json();
+    const weatherApi = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=b806aa9af1c4278561101712e94879ed&lang=ca&units=metric`;
+    const json = yield (yield fetch(weatherApi)).json();
+    const lugar = json.name;
+    const temp = json.main.temp;
+    const icon = json.weather[0].icon;
+    const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+    showIcon.src = iconUrl;
+    mostrarWeather.innerHTML = `${lugar} ${Math.round(temp)} ºC`;
+}))();
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,27 +1,71 @@
+//---> Variables
 const apiUrl = "https://icanhazdadjoke.com";
 const header: any = {
-  method: "GET",
-  headers: {
-    Accept: "application/json",
-  },
+  headers: { Accept: "application/json" },
 };
 
-const show = async () => {
-  const response = await fetch(apiUrl, header);
-  const data = await response.json();
-  const joke: string = data.joke;
-  console.log(joke); // no funciona
-  //document.getElementById("acudit").innerHTML = joke;
-  return joke;
+const mostrarAcudit = <HTMLElement>document.getElementById("acudit");
+const mostrarWeather = <HTMLElement>document.getElementById("weather");
+const showIcon = <HTMLImageElement>document.getElementById("icon");
+const boton: NodeListOf<HTMLButtonElement> = document.querySelectorAll(".btn");
+
+let currentJoke: string = "";
+const reportAcudits: { joke: string; score: number; date: string }[] = [];
+
+const d = new Date();
+let date: string = d.toISOString();
+
+//---> 1a API Jokes
+const showJokes = async () => {
+  boton.forEach((e) => (e.disabled = false));
+  const response = await (await fetch(apiUrl, header)).json();
+  currentJoke = response.joke;
+  mostrarAcudit.innerHTML = currentJoke;
+  console.log(`api1 ${response.joke}`);
 };
 
-/* reportAcudits{
+//---> 2a API Jokes (Chuk Norris)
+const showChuckNorrisJokes = async () => {
+  boton.forEach((e) => (e.disabled = false));
+  const response = await (
+    await fetch("https://api.chucknorris.io/jokes/random")
+  ).json();
+  currentJoke = response.value;
+  mostrarAcudit.innerHTML = currentJoke;
+  console.log(`api2 ${response.value}`);
+};
 
-    joke: "...",
-  
-    score: 1,
-  
-    date: ...
-  
-  
-  } */
+//---> Mostrar acudit alternant API
+let bool: boolean = true;
+const alterna = () => {
+  bool ? showJokes() : showChuckNorrisJokes();
+  bool = !bool;
+};
+
+//---> Puntuar acudits i mostrar per consola (puntuar només una vegada l'acudit)
+function getScore(id: number) {
+  reportAcudits.push({
+    joke: currentJoke,
+    score: id,
+    date: date,
+  });
+  boton.forEach((e) => (e.disabled = true));
+  console.log(reportAcudits);
+}
+
+//---> Mostrar el temps
+const url = "https://ipapi.co/json/";
+
+(async () => {
+  const { latitude, longitude } = await (await fetch(url)).json();
+  const weatherApi = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=b806aa9af1c4278561101712e94879ed&lang=ca&units=metric`;
+  const json = await (await fetch(weatherApi)).json();
+
+  const lugar = json.name;
+  const temp = json.main.temp;
+  const icon = json.weather[0].icon;
+  const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+  showIcon.src = iconUrl;
+
+  mostrarWeather.innerHTML = `${lugar} ${Math.round(temp)} ºC`;
+})();
